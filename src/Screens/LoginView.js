c431764c7734afe2195d5ea9f387dc0486870163
keynanperez <50150611+keynanperez@users.noreply.github.com>
2021-05-24 11:28:06 +0300

    keynan

--- src/Screens/LoginView.js
+++ src/Screens/LoginView.js
@@ -1,158 +1,151 @@
-import React, { useState } from 'react'
-import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import React, { useState } from "react";
+import { TouchableOpacity, StyleSheet, View } from "react-native";
 //import { Text } from 'react-native-paper'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import TextInput from '../components/TextInput'
-import BackButton from '../components/BackButton'
-import { Item, Text } from 'native-base';
-import { theme } from '../core/theme'
-import { ThemeContext ,navigation} from 'react-navigation';
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+import BackButton from "../components/BackButton";
+import { Item, Text } from "native-base";
+import { theme } from "../core/theme";
+import { ThemeContext, navigation } from "react-navigation";
 
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, ThemeConsumer } from 'react-native-elements';
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, ThemeConsumer } from "react-native-elements";
 import myUrl from "./Url";
-import * as Localization from 'expo-localization'
-import color from 'color'
-export default  class  LoginView extends React.Component {
+import * as Localization from "expo-localization";
+import color from "color";
+export default class LoginView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      UserName:"hodaya",
-      UserPass:"aaa",
-      UserId:"",
-      pointsU:0,
-      stageU:1,
-      imgU:"",
-
+      UserName: "hodaya",
+      UserPass: "aaa",
+      UserId: "",
+      pointsU: 0,
+      stageU: 1,
+      imgU: ""
     };
   }
 
-  async componentDidMount  (){
-   
-  
-     this._unsubscribeFocus  =  this.props.navigation.addListener('focus',(payload) =>{
- 
-   
- });
-   }
-  
-  SubmitNote = async() =>{
- let  name=this.state.UserName;
- let pass=this.state.UserPass ;
-  if((name === '')||(pass === ''))
-   {
-        alert("Please insert info!")
-    }
-    else{
-      this.props.navigation.navigate('UserHome',{UserId:this.state.UserId,pointsU:this.state.pointsU,stageU:this.state.stageU,UserName:this.state.UserName,imgU:this.state.imgU});
-    
-      const url = ('http://192.168.0.105:44387/api/'+'Users/')
-
-     
-      const userf = await fetch(url, {
-          method: 'Put',
-          body:   JSON.stringify([name, pass]),
-          headers: new Headers({
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Accept': 'application/json; charset=UTF-8'
-          })
-        })
-        .catch(function(error) {
-          console.log('There has been a problem with your fetch operation: ' + error.message);
-           // ADD THIS THROW error
-            throw error;
-          });
-        const res= await userf.json()
-          if(res.UserName != null)
-            {
-             this.props.navigation.navigate('UserHome',{UserId:res.Id,pointsU:res.Points,stageU:res.UserStage,UserName:res.UserName,imgU:res.Img});
-           };
-          
-            
-          //this.props.navigation.navigate('MyPageView',{UserId:item.UserId})
-           // res.forEach((item) => {
-              //if (item.UserName===name && item.UserPass===pass )
-             // {
-             // 
-               
-             // }
-             
-           // });
-        
-
-       
-
-    }
-    
+  async componentDidMount() {
+    this._unsubscribeFocus = this.props.navigation.addListener(
+      "focus",
+      payload => {}
+    );
   }
 
+  SubmitNote = async () => {
+    let name = this.state.UserName;
+    let pass = this.state.UserPass;
+    if (name === "" || pass === "") {
+      alert("Please insert info!");
+    } else {
+      this.props.navigation.navigate("UserHome", {
+        UserId: this.state.UserId,
+        pointsU: this.state.pointsU,
+        stageU: this.state.stageU,
+        UserName: this.state.UserName,
+        imgU: this.state.imgU
+      });
 
-  render()
-  {
+      const url = "http://192.168.0.105:44387/api/" + "Users/";
 
-   return (
-     <Background>
-       <Logo />
-       <Header>Welcome back.</Header>
-       <TextInput label="Email" style={styles.forgot} />
-       <TextInput style={styles.forgot} label="Password" />
-       <View style={styles.forgotPassword}>
-         <TouchableOpacity
-         onPress={() => this.props.navigation.navigate('ResetPasswordScreen')}
-         >
-           <Text style={styles.link}>Forgot your password?</Text>
-         </TouchableOpacity>
-       </View>
+      const userf = await fetch(url, {
+        method: "Put",
+        body: JSON.stringify([name, pass]),
+        headers: new Headers({
+          "Content-Type": "application/json; charset=UTF-8",
+          Accept: "application/json; charset=UTF-8"
+        })
+      }).catch(function(error) {
+        console.log(
+          "There has been a problem with your fetch operation: " + error.message
+        );
+        // ADD THIS THROW error
+        throw error;
+      });
+      const res = await userf.json();
+      if (res.UserName != null) {
+        this.props.navigation.navigate("UserHome", {
+          UserId: res.Id,
+          pointsU: res.Points,
+          stageU: res.UserStage,
+          UserName: res.UserName,
+          imgU: res.Img
+        });
+      }
 
-       <View style={styles.forgot}>
-         <Text style={styles.forgot}>Don’t have an account? </Text>
-         <TouchableOpacity
-       onPress={() => this.props.navigation.navigate("NewUser")}
-         >
-           <Text style={styles.link}>Sign up</Text>
-         </TouchableOpacity>
-       </View>
+      //this.props.navigation.navigate('MyPageView',{UserId:item.UserId})
+      // res.forEach((item) => {
+      //if (item.UserName===name && item.UserPass===pass )
+      // {
+      //
+
+      // }
+
+      // });
+    }
+  };
+
+  render() {
+    return (
+      <Background>
+        <Logo />
+        <Header>Welcome back.</Header>
+        <TextInput label="Email" style={styles.forgot} />
+        <TextInput style={styles.forgot} label="Password" />
+        <View style={styles.forgotPassword}>
+          <TouchableOpacity
+            onPress={() =>
+              this.props.navigation.navigate("ResetPasswordScreen")
+            }
+          >
+            <Text style={styles.link}>Forgot your password?</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.forgot}>
+          <Text style={styles.forgot}>Don’t have an account? </Text>
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("NewUser")}
+          >
+            <Text style={styles.link}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
         <TouchableOpacity
-       onPress={() => this.props.navigation.navigate("UserHome")}
-         >
-           <Text style={styles.login}>Sign in</Text>
-         </TouchableOpacity>
-     </Background>
-   );}
-
-   }
-   const styles = StyleSheet.create({
-    forgotPassword: {
-      width: '100%',
-      alignItems: 'flex-end',
-      marginBottom: 24,
-      color: '#000000',
-    },
-    row: {
-      flexDirection: 'row',
-      marginTop: 4,
-      color: '#000000',
-    },
-    forgot: {
-      fontSize: 13,
-      color: '#000000',
-    },
-    link: {
-      fontWeight: 'bold',
-      color: theme.colors.primary,
-     },
-    login: {
-      fontSize: 43,
-      color: '#0000ff',
-      fontWeight: 'bold',
-      
-    },
-  })
-  
-
-  
-
-  
+          onPress={() => this.props.navigation.navigate("UserHome")}
+        >
+          <Text style={styles.login}>Sign in</Text>
+        </TouchableOpacity>
+      </Background>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: "100%",
+    alignItems: "flex-end",
+    marginBottom: 24,
+    color: "#000000"
+  },
+  row: {
+    flexDirection: "row",
+    marginTop: 4,
+    color: "#000000"
+  },
+  forgot: {
+    fontSize: 13,
+    color: "#000000"
+  },
+  link: {
+    fontWeight: "bold",
+    color: theme.colors.primary
+  },
+  login: {
+    fontSize: 43,
+    color: "#0000ff",
+    fontWeight: "bold"
+  }
+});
--- src/components/BrandInfo.js
+++ src/components/BrandInfo.js
@@ -6,148 +6,163 @@
  * @flow strict-local
  */
 
- import { Item ,Header} from 'native-base';
-import React, { useState } from 'react';
- import {
-   SafeAreaView,
-   StyleSheet,
-   ScrollView,
-   View,
-   Text,
-   TextInput,
-   Dimensions
- } from 'react-native';
- import {
+import { Item, Header } from "native-base";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+} from "react-native";
+import {
   LineChart,
   BarChart,
   PieChart,
   ProgressChart,
   ContributionGraph,
-  StackedBarChart
+  StackedBarChart,
 } from "react-native-chart-kit";
-import { DataTable } from 'react-native-paper';
- import SearchDropDown from './SearchDropDown'
- import Brands from '../data/Data'
- export default function BrandInfo(props) {
+import { DataTable } from "react-native-paper";
+import SearchDropDown from "./SearchDropDown";
+import Brands from "../data/Data";
+export default function BrandInfo(props) {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          {
+            <View>
+              <Header>
+                <Text style={styles.Header}> {props.dataFromParent}</Text>
+              </Header>
+              <LineChart
+                data={{
+                  labels: [
+                    "Followers",
+                    "tweets",
+                    "Retweets",
+                    "Likes",
+                    "Commends",
+                  ],
+                  datasets: [
+                    {
+                      data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                      ],
+                    },
+                  ],
+                }}
+                width={Dimensions.get("window").width} // from react-native
+                height={220}
+                yAxisLabel=""
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#fb8c00",
+                  backgroundGradientTo: "#ffa726",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) =>
+                    `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16,
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726",
+                  },
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16,
+                }}
+              />
+            </View>
+          }
+        </View>
+      </View>
 
-  
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>
+            {" "}
+            <Text style={styles.Titletable}>Group</Text>
+          </DataTable.Title>
+          <DataTable.Title numeric>
+            {" "}
+            <Text style={styles.Titletable}>popularity</Text>
+          </DataTable.Title>
+          <DataTable.Title numeric>
+            {" "}
+            <Text style={styles.Titletable}>Improvement</Text>{" "}
+          </DataTable.Title>
+        </DataTable.Header>
 
-  
+        <DataTable.Row>
+          <DataTable.Cell>Twitter Api</DataTable.Cell>
+          <DataTable.Cell numeric>159</DataTable.Cell>
+          <DataTable.Cell numeric>-6.0%</DataTable.Cell>
+        </DataTable.Row>
 
-   return (
-     <View style={styles.container}>
- 
-
-       <View style={{ justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-   
-         <View style={{
-           flexWrap: 'wrap', flexDirection: 'row',
-           justifyContent: 'center'
- 
-         }}>
-           
-           {<View>
-            <Header><Text style={styles.Header}> {props.dataFromParent}</Text></Header>
-  <LineChart
-    data={{
-      labels: ["Followers", "tweets", "Retweets", "Likes", "Commends"],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-           
-          ]
-        }
-      ]
-    }}
-    width={Dimensions.get("window").width} // from react-native
-    height={220}
-    yAxisLabel=""
-    yAxisSuffix="k"
-    yAxisInterval={1} // optional, defaults to 1
-    chartConfig={{
-      backgroundColor: "#e26a00",
-      backgroundGradientFrom: "#fb8c00",
-      backgroundGradientTo: "#ffa726",
-      decimalPlaces: 2, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-      propsForDots: {
-        r: "6",
-        strokeWidth: "2",
-        stroke: "#ffa726"
-      }
-    }}
-    bezier
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  />
-
-  
-</View>
+        <DataTable.Row>
+          <DataTable.Cell> Gamares</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+          <DataTable.Cell numeric>8.0%</DataTable.Cell>
+        </DataTable.Row>
+      </DataTable>
+    </View>
+  );
 }
-         </View>
- 
-       </View>
- 
-       <DataTable>
-    <DataTable.Header>
-      <DataTable.Title>Dessert</DataTable.Title>
-      <DataTable.Title numeric>Calories</DataTable.Title>
-      <DataTable.Title numeric>Fat</DataTable.Title>
-    </DataTable.Header>
 
-    <DataTable.Row>
-      <DataTable.Cell>Frozen yogurt</DataTable.Cell>
-      <DataTable.Cell numeric>159</DataTable.Cell>
-      <DataTable.Cell numeric>6.0</DataTable.Cell>
-    </DataTable.Row>
-
-    <DataTable.Row>
-      <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-      <DataTable.Cell numeric>237</DataTable.Cell>
-      <DataTable.Cell numeric>8.0</DataTable.Cell>
-    </DataTable.Row>
-
-  
-  </DataTable>
-     </View>
-   )
- }
- 
- 
- const styles = StyleSheet.create({
-   container: {
-     // justifyContent: 'center',
-     alignItems: 'center',
-     //marginTop: '20%',
-     flex: 1
-   },
-   Header: {
+const styles = StyleSheet.create({
+  container: {
     // justifyContent: 'center',
-    textAlign: 'center',
-    marginTop: 20,
-    fontSize:30
+    alignItems: "center",
+    //marginTop: '20%',
+    flex: 1,
+  },
+  Header: {
+    // justifyContent: 'center',
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 40,
+    fontWeight: "bold",
     //flex: 1
   },
-   textInput: {
-     backgroundColor: '#BFBFBF',
-     width: '80%',
-     borderRadius: 5,
-     height: 50,
-     fontSize: 20,
-     fontWeight: 'bold',
-     paddingHorizontal: 10,
-   },
- });
- 
- 
+  textInput: {
+    backgroundColor: "#BFBFBF",
+    width: "100%",
+    borderRadius: 5,
+    height: 50,
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingHorizontal: 10,
+  },
+  Titletable: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#B62F2F",
+  },
+});
